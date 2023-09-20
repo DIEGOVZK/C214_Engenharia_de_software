2075449826cbec103668c945c36d314f460abf32
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-09-20 18:02:28 -0300

    testing file extension management by the class ➕

--- todo/test/persist.test.js
+++ todo/test/persist.test.js
@@ -1,4 +1,4 @@
-import { FileHandler } from '../src/persist';
+import { FileHandler } from '../src/persist.js';
 import { expect } from 'chai';
 
 describe('FileHandler', () => {
@@ -9,9 +9,25 @@ describe('FileHandler', () => {
         fileHandler = new FileHandler(clientId);
     });
 
-    describe('#getFileName', () => {
+    describe('---- basic API ----', () => {
         it('should have an object of type FileHandler inside fileHandler', () => {
             expect(fileHandler).to.be.an.instanceOf(FileHandler);
         });
     });
+
+    describe('---- getFileName ----', () => {
+        it('should return the correct file name with the default extension', () => {
+            const expectedFileName = 'testClient.txt'; // Updated expected file name
+            const result = fileHandler.getFileName();
+            expect(result).to.equal(expectedFileName);
+        });
+
+        it('should return the correct file name with a custom extension', () => {
+            const customExtension = 'csv';
+            const expectedFileName = 'testClient.csv'; // Updated expected file name
+            const result = fileHandler.getFileName(customExtension);
+            expect(result).to.equal(expectedFileName);
+        });
+    });
+
 });
