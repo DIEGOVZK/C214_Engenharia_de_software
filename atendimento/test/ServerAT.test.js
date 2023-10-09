1a5ce0623896d5e2f95012afb35049eaf7b9c1d6
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-10-09 18:47:54 -0300

    added query result testing 🚧➕

--- atendimento/test/ServerAT.test.js
+++ atendimento/test/ServerAT.test.js
@@ -18,4 +18,31 @@ describe('ServerAT', () => {
         expect(serverAT).to.be.an.instanceOf(ServerAT);
     });
 
+    it('should correctly parse and return professor data from the database', async () => {
+        // Mock a database response
+        const expectedResult = [
+            {
+                nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
+                horarioDeAtendimento: 'Terça 19:30 - 21:10',
+                periodo: 'Noturno',
+                sala: '3',
+                predio: '1',
+            },
+            {
+                nomeDoProfessor: 'Edson Josias Cruz Gimenez',
+                horarioDeAtendimento: 'Quarta 15:30 - 17:10',
+                periodo: 'Integral',
+                sala: '2',
+                predio: '1',
+            },
+        ];
+
+        databaseMock.query.resolves(expectedResult);
+
+        const building = 1;
+        const result = await serverAT.loadFromBuilding(building);
+
+        expect(result).to.deep.equal(expectedResult);
+    });
+
 });
