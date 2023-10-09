5a7cc20a2ea0b8fc6b359b443598b7bf7c8e385c
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-10-09 20:37:24 -0300

    added sad path for returned empty array ➕✔️

--- atendimento/test/Hrat.test.js
+++ atendimento/test/Hrat.test.js
@@ -22,50 +22,73 @@ describe('Hrat', () => {
         it('should be instantiated with a ServerAT instance', () => {
             expect(hrat.server).to.equal(serverATMock);
         });
+
+        it('should load professors correctly from the server', () => {
+            serverATMock.loadFromBuilding.returns([
+                {
+                    nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
+                    horarioDeAtendimento: 'Terça 19:30 - 21:10',
+                    periodo: 'Noturno',
+                    sala: '3',
+                    predio: '1',
+
+                },
+                {
+                    nomeDoProfessor: 'Edson Josias Cruz Gimenez',
+                    horarioDeAtendimento: 'Quarta 15:30 - 17:10',
+                    periodo: 'Integral',
+                    sala: '2',
+                    predio: '1',
+
+                },
+            ]);
+
+            const expectedResult = [
+                {
+                    nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
+                    horarioDeAtendimento: 'Terça 19:30 - 21:10',
+                    periodo: 'Noturno',
+                    sala: '3',
+                    predio: '1',
+
+                },
+                {
+                    nomeDoProfessor: 'Edson Josias Cruz Gimenez',
+                    horarioDeAtendimento: 'Quarta 15:30 - 17:10',
+                    periodo: 'Integral',
+                    sala: '2',
+                    predio: '1',
+
+                },
+            ];
+
+            const sala = 8;
+            const result = hrat.loadFromServer(sala);
+
+            expect(result).to.deep.equal(expectedResult);
+        });
+
+        describe('Hrat (happy path)', () => {
+            it('should handle an empty result from the server (sad path)', () => {
+                serverATMock.loadFromBuilding.returns([]);
+
+                const sala = 6;
+                const result = hrat.loadFromServer(sala);
+
+                expect(result).to.deep.equal([]);
+            });
+        });
     });
 
-    it('should load professors correctly from the server', () => {
-        serverATMock.loadFromBuilding.returns([
-            {
-                nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
-                horarioDeAtendimento: 'Terça 19:30 - 21:10',
-                periodo: 'Noturno',
-                sala: '3',
-                predio: '1',
 
-            },
-            {
-                nomeDoProfessor: 'Edson Josias Cruz Gimenez',
-                horarioDeAtendimento: 'Quarta 15:30 - 17:10',
-                periodo: 'Integral',
-                sala: '2',
-                predio: '1',
 
-            },
-        ]);
+    it('should handle an empty result from the server (sad path)', () => {
+        // Mock the serverAT query response as empty
+        serverATMock.loadFromBuilding.returns([]);
 
-        const expectedResult = [
-            {
-                nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
-                horarioDeAtendimento: 'Terça 19:30 - 21:10',
-                periodo: 'Noturno',
-                sala: '3',
-                predio: '1',
-
-            },
-            {
-                nomeDoProfessor: 'Edson Josias Cruz Gimenez',
-                horarioDeAtendimento: 'Quarta 15:30 - 17:10',
-                periodo: 'Integral',
-                sala: '2',
-                predio: '1',
-
-            },
-        ];
-
-        const sala = 8;
+        const sala = 6;
         const result = hrat.loadFromServer(sala);
 
-        expect(result).to.deep.equal(expectedResult);
+        expect(result).to.deep.equal([]);
     });
 });
