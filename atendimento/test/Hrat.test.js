4205acdd71e43b048c4bb5d73c7835762ea0b24d
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-10-09 20:35:34 -0300

    added test to get the list from the server to use

---
+++ atendimento/test/Hrat.test.js
@@ -0,0 +1,71 @@
+import ServerAT from '../src/ServerAT.js';
+import Hrat from '../src/Hrat.js';
+import { expect } from 'chai';
+import sinon from 'sinon';
+
+describe('Hrat', () => {
+    let hrat;
+    let serverATMock;
+
+    beforeEach(() => {
+        serverATMock = {
+            loadFromBuilding: sinon.stub(),
+        };
+        hrat = new Hrat(serverATMock);
+    });
+
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    describe('Hrat (happy path)', () => {
+        it('should be instantiated with a ServerAT instance', () => {
+            expect(hrat.server).to.equal(serverATMock);
+        });
+    });
+
+    it('should load professors correctly from the server', () => {
+        serverATMock.loadFromBuilding.returns([
+            {
+                nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
+                horarioDeAtendimento: 'Terça 19:30 - 21:10',
+                periodo: 'Noturno',
+                sala: '3',
+                predio: '1',
+
+            },
+            {
+                nomeDoProfessor: 'Edson Josias Cruz Gimenez',
+                horarioDeAtendimento: 'Quarta 15:30 - 17:10',
+                periodo: 'Integral',
+                sala: '2',
+                predio: '1',
+
+            },
+        ]);
+
+        const expectedResult = [
+            {
+                nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
+                horarioDeAtendimento: 'Terça 19:30 - 21:10',
+                periodo: 'Noturno',
+                sala: '3',
+                predio: '1',
+
+            },
+            {
+                nomeDoProfessor: 'Edson Josias Cruz Gimenez',
+                horarioDeAtendimento: 'Quarta 15:30 - 17:10',
+                periodo: 'Integral',
+                sala: '2',
+                predio: '1',
+
+            },
+        ];
+
+        const sala = 8;
+        const result = hrat.loadFromServer(sala);
+
+        expect(result).to.deep.equal(expectedResult);
+    });
+});
