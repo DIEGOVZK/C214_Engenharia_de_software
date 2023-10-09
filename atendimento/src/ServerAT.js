d5e96a8633fab544c355c9513ed7053d27bc9f9e
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-10-09 18:44:09 -0300

    updated default_obj strcture to fix bad code 🚧💀

--- atendimento/src/ServerAT.js
+++ atendimento/src/ServerAT.js
@@ -2,29 +2,27 @@ import postgres from 'postgres';
 import path from 'path';
 import fs from 'fs';
 
-const defaultDatabase = new Database();
+const options = {
+    host: 'localhost',
+    port: 5432,
+
+    database: 'server-atendimento-database',
+    username: 'server-atendimento-database-user',
+    password: 'server-atendimento-database-password',
+};
+
+const defaultDatabase = postgres(options);
 
 class ServerAT {
 
     // Classe desacoplada de Database
     constructor(db_obj = defaultDatabase) {
-        db = db_obj;
+        this.db = db_obj;
     }
-
-    options = {
-        host: 'localhost',
-        port: 5432,
-
-        database: 'server-atendimento-database',
-        username: 'server-atendimento-database-user',
-        password: 'server-atendimento-database-password',
-    };
-
-    sql = postgres(this.options);
 
     async query(query) {
         try {
-            const result = await this.sql.unsafe(query).then((res) => Object.values(res));
+            const result = await this.db.unsafe(query).then((res) => Object.values(res));
             return result;
         } catch (error) {
             throw error;
@@ -33,12 +31,10 @@ class ServerAT {
 
     async loadFromBuilding(building) {
         let result = await this.query(`SELECT getlistbybuilding (${building})`);
-        
         let data = [];
         result.forEach(element => {
             data.push(result.getlistbybuilding)
         });
-
         return data;
     }
 }
