f99d78b18a86d2a526377d72b18f97f6621596e3
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-10-09 18:22:56 -0300

    USING TDD, no need for main now

--- atendimento/src/Main.js
+++ atendimento/src/Main.js
@@ -2,3 +2,5 @@ import PromptSync from "prompt-sync";
 
 console.log("\n-------- horários de atendimento dos professores --------\n");
 
+// ---- TDD - CONSIDERANDO QUE AS CLASSES SERÃO TESTADAS E VALIDADAS ---- //
+// ---- SEM NECESSIDADE DA MAIN COM O CÓDIGO FUNCIONAL POR ENQUANTO ---- //
