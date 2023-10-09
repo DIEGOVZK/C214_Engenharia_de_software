8a22fbed9ebbb2b467589c2058d134defd8479db
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2023-10-09 18:57:57 -0300

    passing query parsing of returned elemets ✔️

--- atendimento/test/ServerAT.test.js
+++ atendimento/test/ServerAT.test.js
@@ -2,44 +2,70 @@ import ServerAT from '../src/ServerAT.js';
 import { expect } from 'chai';
 import sinon from 'sinon';
 
-// Mock database usando Sinon
-const databaseMock = {
-    query: sinon.stub(),
-};
-
-const serverAT = new ServerAT(databaseMock);
-
 describe('ServerAT', () => {
+    let serverAT;
+    let dbMock;
+
+    beforeEach(() => {
+        // Mock database usando Sinon
+        dbMock = {
+            unsafe: sinon.stub(),
+        };
+
+        serverAT = new ServerAT(dbMock);
+    });
+
     afterEach(() => {
-        sinon.reset();
+        sinon.restore();
     });
 
     it('should be an instance of ServerAT', () => {
         expect(serverAT).to.be.an.instanceOf(ServerAT);
     });
 
-    it('should correctly parse and return professor data from the database', async () => {
-        // Mock a database response
-        const expectedResult = [
+    it('should parse the result from the database correctly (happy path)', async () => {
+
+        const queryResult = [
             {
-                nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
-                horarioDeAtendimento: 'Terça 19:30 - 21:10',
-                periodo: 'Noturno',
-                sala: '3',
-                predio: '1',
+                "getlistbybuilding": {
+                    nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
+                    horarioDeAtendimento: 'Terça 19:30 - 21:10',
+                    periodo: 'Noturno',
+                    sala: '3',
+                    predio: '1',
+                }
             },
             {
-                nomeDoProfessor: 'Edson Josias Cruz Gimenez',
-                horarioDeAtendimento: 'Quarta 15:30 - 17:10',
-                periodo: 'Integral',
-                sala: '2',
-                predio: '1',
+                "getlistbybuilding": {
+                    nomeDoProfessor: 'Edson Josias Cruz Gimenez',
+                    horarioDeAtendimento: 'Quarta 15:30 - 17:10',
+                    periodo: 'Integral',
+                    sala: '2',
+                    predio: '1',
+                }
             },
         ];
 
-        databaseMock.query.resolves(expectedResult);
+        const expectedResult = [
+            {
+                    nomeDoProfessor: 'João Pedro Magalhães de Paula Paiva',
+                    horarioDeAtendimento: 'Terça 19:30 - 21:10',
+                    periodo: 'Noturno',
+                    sala: '3',
+                    predio: '1',
+            },
+            {
+                    nomeDoProfessor: 'Edson Josias Cruz Gimenez',
+                    horarioDeAtendimento: 'Quarta 15:30 - 17:10',
+                    periodo: 'Integral',
+                    sala: '2',
+                    predio: '1',
+            },
+        ];
 
-        const building = 1;
+        dbMock.unsafe.resolves(queryResult);
+
+        const building = '1';
         const result = await serverAT.loadFromBuilding(building);
 
         expect(result).to.deep.equal(expectedResult);
